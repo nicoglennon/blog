20c291f4aafe410ab4ab0cb1ca0e9ec779f00019
Nico Glennon <nicoglennon@icloud.com>
2023-02-10 22:11:49 -0500

    initial commit

---
+++ main.tsx
@@ -0,0 +1,27 @@
+/** @jsx h */
+
+import blog, { ga, redirects, h } from "blog";
+
+blog({
+  title: "My Blog",
+  description: "This is my new blog.",
+  // header: <header>Your custom header</header>,
+  // section: <section>Your custom section</section>,
+  // footer: <footer>Your custom footer</footer>,
+  avatar: "https://deno-avatar.deno.dev/avatar/blog.svg",
+  avatarClass: "rounded-full",
+  author: "An author",
+
+  // middlewares: [
+
+    // If you want to set up Google Analytics, paste your GA key here.
+    // ga("UA-XXXXXXXX-X"),
+
+    // If you want to provide some redirections, you can specify them here,
+    // pathname specified in a key will redirect to pathname in the value.
+    // redirects({
+    //  "/hello_world.html": "/hello_world",
+    // }),
+
+  // ]
+});
---
+++ posts/hello_world.md
@@ -0,0 +1,6 @@
+---
+title: Hello world!
+publish_date: 2023-02-11
+---
+
+This is my first blog post!
